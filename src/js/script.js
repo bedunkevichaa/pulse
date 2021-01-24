3eb0b9ef936cedc34dd4601998c15600a54898bd
Anton <bedunkevichaa@gmail.com>
2021-01-24 22:09:48 +0300

    d4_slider

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,19 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev">img src="../img/left.png"</button>'
-      });
+        //adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png"></button>',
+         responsive: [
+		 	{
+		 		breakpoint: 992,
+		 		settings: {
+		 			dots: true,
+		 			arrows: false
+		 		}
+		 	}
+        ]
+    });
   });
 
 //number
